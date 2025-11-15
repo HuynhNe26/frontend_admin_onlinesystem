4237ce57dddc30f53a0ed80d3d4c31f9c5e8a548
HuynhNe26 <nguyenhoanghuynh.vn@gmail.com>
2025-11-15 10:03:03 +0700

    update

--- admin/src/components/layout.js
+++ admin/src/components/layout.js
@@ -3,10 +3,17 @@ import Navbar from "./navbar/navbar";
 
 export default function Layout() {
   return (
-    <div style={{ display: "flex", minHeight: "100vh" }}>
+    <div style={{ display: "flex", height: "100vh", overflow: "hidden" }}>
       <Navbar />
 
-      <main style={{ flex: 1, padding: "20px" }}>
+      <main
+        style={{
+          flex: 1,
+          padding: "20px",
+          overflowY: "auto",
+          height: "100vh"
+        }}
+      >
         <Outlet />
       </main>
     </div>
