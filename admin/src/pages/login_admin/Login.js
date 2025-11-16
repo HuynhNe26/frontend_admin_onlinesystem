e582374f7a62c652d64c76975ef0d2537b98e4cf
HuynhNe26 <nguyenhoanghuynh.vn@gmail.com>
2025-11-16 17:55:33 +0700

    update

--- admin/src/pages/login_admin/Login.js
+++ admin/src/pages/login_admin/Login.js
@@ -22,7 +22,7 @@ export default function Login() {
     setErr("");
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/api/admin/login", {
+      const res = await fetch("https://backend-onlinesystem.onrender.com/api/admin/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
